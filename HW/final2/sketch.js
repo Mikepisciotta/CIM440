e951f9b9909eaf2cac12984eb504e7947739ed6b
Mikepisciotta <51210519+Mikepisciotta@users.noreply.github.com>
2019-11-18 12:37:46 -0500

    new assignment

---
+++ HW/final2/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
+
+//hold a to jump and prolong jump
+//jump over series of different sized holes and random obstacles
+//reset when making contact with hole or objects
